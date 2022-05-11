666d14d618119b39cf1dc5beead4f278f15ec9b4
Nicholas-Mercado <Nicholasamerc@gmail.com>
2022-05-10 17:39:28 -0700

    welcome and input

--- scraper.py
+++ scraper.py
@@ -1,11 +1,21 @@
 import requests
 from bs4 import BeautifulSoup
 
+
+print(""" 
+        --> Welcome to Citation Station <--
+        
+        Input a Wikipedia URL and you
+        will get back a how many citations
+        needed on that page and list of
+        those citations 
+      
+""")
+wiki_url = input("> ")
+page = requests.get(wiki_url)
+    
 def get_citations_needed_count():
     count_of_cites=0
-    
-    URL = "https://en.wikipedia.org/wiki/History_of_Mexico"
-    page = requests.get(URL)
 
     soup = BeautifulSoup(page.content, "html.parser")
     results = soup.find_all(title = 'Wikipedia:Citation needed')
